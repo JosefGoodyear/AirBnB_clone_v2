93eee614a8ff8e1c95e3ca590fc64c90c56176f7
JosefGoodyear <josefgoodyear@gmail.com>
2019-08-13 17:38:42 +0000

    halfway through #6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Database Storage Engine """
+from sqlalchemy import (create_engine)
+
+
+class DBStorage:
+    """
+    This class is the database storage engine
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """
+        Initialize an instance
+        """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
+        if HBNB_ENV == 'test':
+            Base.metadata.drop_all(engine)
+
+    def all(self, cls=None):
+        """
+        query all objects from the current db session, based on class name
+        """
+
+
