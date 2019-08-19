9b8e4544cb4c5e51bc2c792bf367cf7db7073fd6
JosefGoodyear <josefgoodyear@gmail.com>
2019-08-19 18:16:20 +0000

    update config file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -27,3 +27,5 @@ printf "
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
+sed -i "/server_name _;/a location /hbnb_static/ {\n\t alias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+service nginx restart
