a727f6b8d00d043e227c1d2241afffc825d028a2
JosefGoodyear <josefgoodyear@gmail.com>
2019-08-19 17:36:52 +0000

    add unfinished script

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Set up web servers for deployment
+apt-get update
+apt-get install nginx
+if [ ! -d /data/ ]; then
+	mkdir /data/
+fi
+if [ ! -d /data/web_static ]; then
+	mkdir /data/web_static/
+fi
+if [ ! -d /data/web_static/releases/ ]; then
+	mkdir /data/web_static/releases/
+fi
+if [ ! -d /data/web_static/shared/ ]; then
+	mkdir /data/web_static/shared/
+fi
+if [ ! -d /data/web_static/releases/test/ ]; then
+	mkdir /data/web_static/releases/test/
+fi
+printf "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
