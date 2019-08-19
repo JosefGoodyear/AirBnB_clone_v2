16da579fee439b2bce11b09213db415b203c9341
JosefGoodyear <josefgoodyear@gmail.com>
2019-08-19 20:54:02 +0000

    change web page

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -28,7 +28,7 @@ printf "<html>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>\n" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/server_name _;/a location /hbnb_static {\n\t alias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
