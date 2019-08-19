2c88585d7097ea49d8a960625bb0b063124f0ee4
JosefGoodyear <josefgoodyear@gmail.com>
2019-08-19 20:39:22 +0000

    add documentation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+""" archive web_static for easy shipping """
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
+    """ archive web_static """
     time_now = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    local("tar -czvf versions/web_static_{}.tgz web_static/".format(time_now))
+    check = local("tar -czvf versions/web_static_{}.tgz web_static/"
+                  .format(time_now))
+    if check.succeeded:
+        return check
