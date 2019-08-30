9497e1fa1c26893c34e1233d2d47c23819ddae1d
JosefGoodyear <josefgoodyear@gmail.com>
2019-08-30 00:14:25 +0000

    add second getter

--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ from os import getenv
 import models
 from models.city import City
 
+
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
@@ -18,13 +19,20 @@ class State(BaseModel, Base):
                           cascade="all, delete-orphan",
                           backref="state")
 
-
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             city_list = []
 
             for city in models.storage.all(City).values():
+                if self.id == city.state_id:
+                    city_list.append(city)
+            return city_list
+    else:
+        @property
+        def cities(self):
+            city_list = []
+            for city in self.cities:
                 if self.id == city.state_id:
                     city_list.append(city)
             return city_list
