b08a019eac6604eaa00e5fb9a99681eb25e74b06
JosefGoodyear <josefgoodyear@gmail.com>
2019-09-01 22:44:25 +0000

    fix files for pep8 part 2

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,6 +23,7 @@ def c(text):
     s = s.replace('_', ' ')
     return s
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is_cool'):
